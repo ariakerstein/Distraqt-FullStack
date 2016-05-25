b69ef5a65c46573d47ecf4c4c35f0c3f0617f95b
aakerst <aakerstein@walmart.com>
2016-05-24 22:23:48 -0700

    update GA

--- distraqt_finalProject.py
+++ distraqt_finalProject.py
@@ -294,14 +294,15 @@ def restaurantsJSON():
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
-    # if 'username' not in login_session:
-    #     return redirect('/welcomeSplash')
+    if 'username' not in login_session:
+        return redirect('/welcomeSplash')
     # if 'user.name' == 'restaurant.user':
     # if username not in restaurant.user_id:
     #     return redirect('/login')
     # if restaurant.user == user.id:
     # if'restaurant.user_id'=='username':
-    restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
+    restaurants = session.query(Restaurant).join(User).filter_by(Restaurant.User_id=1)
+    # order_by(asc(Restaurant.name))
     return render_template('d_restaurants.html', restaurants=restaurants)
 
 
