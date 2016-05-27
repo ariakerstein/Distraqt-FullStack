2e2ee53f82fc809f25886cf248705e9382de2b53
aakerst <aakerstein@walmart.com>
2016-05-26 21:04:27 -0700

    updated routes to use /restaurant - workaround for the short-term until all urls are updated

--- distraqt_finalProject.py
+++ distraqt_finalProject.py
@@ -270,7 +270,7 @@ def gdisconnect():
 
 
 # JSON APIs to view Restaurant Information
-@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+@app.route('/distraqt/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(
@@ -278,13 +278,13 @@ def restaurantMenuJSON(restaurant_id):
     return jsonify(MenuItems=[i.serialize for i in items])
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+@app.route('/distraqt/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
     Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
     return jsonify(Menu_Item=Menu_Item.serialize)
 
 
-@app.route('/restaurant/JSON')
+@app.route('/distraqt/JSON')
 def restaurantsJSON():
     restaurants = session.query(Restaurant).all()
     return jsonify(restaurants=[r.serialize for r in restaurants])
@@ -293,6 +293,7 @@ def restaurantsJSON():
 # Show all restaurants
 @app.route('/')
 @app.route('/distraqt/')
+@app.route('/restaurant/')
 def showRestaurants():
     if 'username' not in login_session:
         return redirect('/welcome')
