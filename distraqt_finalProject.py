026584abb3b38ec1edc8831db2f90c5000b1eba7
aakerst <aakerstein@walmart.com>
2016-06-08 12:52:02 -0700

    removed fb auth login - cleanup comments

--- distraqt_finalProject.py
+++ distraqt_finalProject.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import xrange
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
@@ -25,12 +27,6 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 # reinstate sqlite for final project review purposes:
 engine = create_engine('sqlite:///distraqtJun6.db')
-### Deprecated sqlite dbs used for testing: ###
-# Connect to Database and create database session
-# engine = create_engine('sqlite:///distraqtJan5.db')
-# engine = create_engine('sqlite:///distraqtDecember27.db')
-# engine = create_engine('sqlite:///distraqtDecember27.db')
-# engine = create_engine('sqlite:///distraqtFeb25.db')
 
 # functional postgres db - commented out for final project submission purposes
 ### Create postgres db with the following name ###
@@ -49,84 +45,6 @@ def showLogin():
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
-
-### Note: deprecated FB auth code below ###
-# @app.route('/fbconnect', methods=['POST'])
-# def fbconnect():
-#     if request.args.get('state') != login_session['state']:
-#         response = make_response(json.dumps('Invalid state parameter.'), 401)
-#         response.headers['Content-Type'] = 'application/json'
-#         return response
-#     access_token = request.data
-#     print "access token received %s " % access_token
-
-#     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-#         'web']['app_id']
-#     app_secret = json.loads(
-#         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-#     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-#         app_id, app_secret, access_token)
-#     h = httplib2.Http()
-#     result = h.request(url, 'GET')[1]
-
-#     # Use token to get user info from API
-#     userinfo_url = "https://graph.facebook.com/v2.4/me"
-#     # strip expire tag from access token
-#     token = result.split("&")[0]
-
-
-#     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
-#     h = httplib2.Http()
-#     result = h.request(url, 'GET')[1]
-#     # print "url sent for API access:%s"% url
-#     # print "API JSON result: %s" % result
-#     data = json.loads(result)
-#     login_session['provider'] = 'facebook'
-#     login_session['username'] = data["name"]
-#     login_session['email'] = data["email"]
-#     login_session['facebook_id'] = data["id"]
-
-#     # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
-#     stored_token = token.split("=")[1]
-#     login_session['access_token'] = stored_token
-
-#     # Get user picture
-#     url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
-#     h = httplib2.Http()
-#     result = h.request(url, 'GET')[1]
-#     data = json.loads(result)
-
-#     login_session['picture'] = data["data"]["url"]
-
-#     # see if user exists
-#     user_id = getUserID(login_session['email'])
-#     if not user_id:
-#         user_id = createUser(login_session)
-#     login_session['user_id'] = user_id
-
-#     output = ''
-#     output += '<h1>Welcome, '
-#     output += login_session['username']
-
-#     output += '!</h1>'
-#     output += '<img src="'
-#     output += login_session['picture']
-#     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
-#     flash("Now logged in as %s" % login_session['username'])
-#     return output
-
-### Note: deprecated FB auth ###
-# @app.route('/fbdisconnect')
-# def fbdisconnect():
-#     facebook_id = login_session['facebook_id']
-#     # The access token must me included to successfully logout
-#     access_token = login_session['access_token']
-#     url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
-#     h = httplib2.Http()
-#     result = h.request(url, 'DELETE')[1]
-#     return "you have been logged out"
-
 
 
 @app.route('/gconnect', methods=['POST'])
